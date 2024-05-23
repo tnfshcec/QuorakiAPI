tnfshcec/QuorakiAPI

commit 65bd8a9cd13adaaf7be128a2edad0faf4a465163

add swagger for api reference
* added @elysiajs/swagger to generate api reference page
* fixed typing (`Literal` -> `Enum`)

src/ranking.ts
@@ -23,16 +23,25 @@ export const RANKING_TYPES = {
     },
 };
 
+// INFO: `Object.keys()` doesn't return the keys as type for reasons,
+// but it should be safe here so we're overridding the type
 const RANKING_TYPE_KEYS = Object.keys(RANKING_TYPES) as unknown as Array<
     keyof typeof RANKING_TYPES
 >;
 
-export const RankArgsType = t.Object({
-    type: t.Optional(t.Union(RANKING_TYPE_KEYS.map((type) => t.Literal(type)))),
+// INFO: This function is from TypeBox's docs,
+// to generate an OpenAPI enum with a string array.
+const StringEnum = <T extends string[]>(values: [...T]) =>
+    t.Unsafe<T[number]>({
+        type: "string",
+        enum: values,
+    });
+
+export const RankArgs = t.Object({
+    type: t.Optional(StringEnum(RANKING_TYPE_KEYS)),
     text: t.String(),
 });
-
-export type RankArgs = Static<typeof RankArgsType>;
+export type RankArgs = Static<typeof RankArgs>;
 
 export function rank({ type = "quote", text }: RankArgs) {
     let ranking = RANKING_TYPES[type];

src/index.ts
@@ -1,10 +1,11 @@
 import { Elysia } from "elysia";
-import { rank, RankArgsType } from "./ranking";
+import { swagger } from "@elysiajs/swagger";
+import { rank, RankArgs } from "./ranking";
 
 const app = new Elysia()
-    .get("/", () => "Hello Elysia")
+    .use(swagger())
     .get("/api/rank", ({ query }) => rank(query), {
-        query: RankArgsType,
+        query: RankArgs,
     })
     .listen(3000);
 

commit 8f23fce68215157bfeff32b1e97e46d15def85c6

add dockerfile
* added dockerfile for container
* fixed `Unknown Type` error caused by 65bd8a9c

Dockerfile
@@ -1,0 +1,17 @@
+FROM oven/bun
+
+WORKDIR /app
+
+COPY package.json .
+COPY bun.lockb .
+
+RUN bun install --production
+
+COPY src src
+COPY tsconfig.json .
+# COPY public public
+
+ENV NODE_ENV production
+CMD ["bun", "src/index.ts"]
+
+EXPOSE 3000

src/ranking.ts
@@ -29,16 +29,8 @@ const RANKING_TYPE_KEYS = Object.keys(RANKING_TYPES) as unknown as Array<
     keyof typeof RANKING_TYPES
 >;
 
-// INFO: This function is from TypeBox's docs,
-// to generate an OpenAPI enum with a string array.
-const StringEnum = <T extends string[]>(values: [...T]) =>
-    t.Unsafe<T[number]>({
-        type: "string",
-        enum: values,
-    });
-
 export const RankArgs = t.Object({
-    type: t.Optional(StringEnum(RANKING_TYPE_KEYS)),
+    type: t.Optional(t.Union(RANKING_TYPE_KEYS.map((k) => t.Literal(k)))),
     text: t.String(),
 });
 export type RankArgs = Static<typeof RankArgs>;