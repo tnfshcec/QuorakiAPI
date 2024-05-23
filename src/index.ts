fa746b3a1a6f10951217e8161db596b1011ae1a4
Eggrror404 <94736451+Eggrror404@users.noreply.github.com>
2024-05-24 01:07:29 +0800

    update swagger configuration

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,29 @@ import { swagger } from "@elysiajs/swagger";
 import { rank, RankArgs } from "./ranking";
 
 const app = new Elysia()
-    .use(swagger())
+    .use(
+        swagger({
+            documentation: {
+                info: {
+                    title: "QuorakiAPI",
+                    version: "1.0.0",
+                    description:
+                        "QuorakiAPI documentation, made with 🐔 in TNFSHCEC.",
+                    license: {
+                        name: "MIT License",
+                        url: "https://github.com/tnfshcec/QuorakiAPI/blob/main/LICENSE",
+                    },
+                },
+            },
+        }),
+    )
     .get("/api/rank", ({ query }) => rank(query), {
         query: RankArgs,
     })
     // Process 404 error
-   .onError(({ code }) => {
-        if (code === 'NOT_FOUND') {
-           return { error: '😆狗屁不通不知道哪個sb想到的路徑😆' };
+    .onError(({ code }) => {
+        if (code === "NOT_FOUND") {
+            return { error: "😆狗屁不通不知道哪個sb想到的路徑😆" };
         }
     })
     .listen(3000);
